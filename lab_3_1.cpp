8f101e9dedfe358082a24bc68b8b496ed15a9c37
Unknown <Chonthichayon@cvc-cha.ac.th>
2017-11-24 14:04:42 +0700

    ข้อ1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout << "Miss." << endl ;
+  cout << "Chonthicha Yongseng" << endl;
+  cout << "Gam" << endl;
+  cout << "Femal" << endl;
+  cout << "14/02/42" << endl;
+  cout << "playgame" << endl;
+  cout << "singel" << endl;
   return 0;
 }
